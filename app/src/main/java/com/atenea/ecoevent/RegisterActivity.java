b2f6734dcc1c648f8c1a71f35a44c93e868b2755
SantiagoNoguera <santi.nog23@gmail.com>
2023-12-01 21:57:23 -0500

    Implentación de la vista de registro.

--- app/src/main/java/com/atenea/ecoevent/RegisterActivity.java
+++ app/src/main/java/com/atenea/ecoevent/RegisterActivity.java
@@ -2,7 +2,10 @@ package com.atenea.ecoevent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //Referencias a elementos de la vista.
+        TextView loginTV = findViewById(R.id.tv_login);
+
+        //Referencias a las demás actividades.
+        Intent loginView = new Intent(this, LoginActivity.class);
+
+        //Evento de click para enlace a la vista de inicio de sesión.
+        loginTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(loginView);
+            }
+        });
     }
 }
