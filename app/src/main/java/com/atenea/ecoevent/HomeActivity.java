f08ff68d918fb969b1be32a3417d06be3c126452
SantiagoNoguera <santi.nog23@gmail.com>
2023-12-05 23:21:57 -0500

    Implentación de navegación al interior de la aplicación.

--- app/src/main/java/com/atenea/ecoevent/HomeActivity.java
+++ app/src/main/java/com/atenea/ecoevent/HomeActivity.java
@@ -1,8 +1,16 @@
 package com.atenea.ecoevent;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +18,37 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //Configuración de la barra inferior.
+        BottomAppBar bottomAppBar = findViewById(R.id.bottom_app_bar);
+        setSupportActionBar(bottomAppBar);
+
+        //Configuración de la navegación de la barra inferior.
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent view;
+
+                switch (item.getTitle().toString()) {
+                    case "Categorias":
+                        view = new Intent(HomeActivity.this, CategoriesActivity.class);
+                        break;
+                    case "Estadísticas":
+                        view = new Intent(HomeActivity.this, StatisticsActivity.class);
+                        break;
+                    case "Consejos":
+                        view = new Intent(HomeActivity.this, TipsActivity.class);
+                        break;
+                    default:
+                        view = new Intent(HomeActivity.this, HomeActivity.class);
+                        break;
+                }
+
+                startActivity(view);
+                return false;
+            }
+        });
+
     }
 }
--- app/src/main/java/com/atenea/ecoevent/StatisticsActivity.java
+++ app/src/main/java/com/atenea/ecoevent/StatisticsActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class EstadisticsActivity extends AppCompatActivity {
+public class StatisticsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_estadistics);
+        setContentView(R.layout.activity_stadistics);
     }
 }
