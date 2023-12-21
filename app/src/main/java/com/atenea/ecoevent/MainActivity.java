5ca20fd3f5294c91822569db5e4e5b6bf3cc7459
SantiagoNoguera <santi.nog23@gmail.com>
2023-12-20 20:40:39 -0500

    Lógica de la vista de consejos.

--- app/src/main/java/com/atenea/ecoevent/MainActivity.java
+++ app/src/main/java/com/atenea/ecoevent/MainActivity.java
@@ -5,14 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.atenea.ecoevent.models.Event;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,6 +38,28 @@ public class MainActivity extends AppCompatActivity {
             writer.append("Evento 1,01/01/2023,1,2,3,4,5\n");
             writer.append("Evento 2,02/02/2023,10,2,3,5,5\n");
             writer.append("Evento 3,03/03/2023,6,2,8,4,5\n");
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Crear archivo para almacenar los consejos que se mostrarán.
+        File tips = new File(getFilesDir(), "tips.txt");
+        try {
+            FileWriter writer = new FileWriter(tips);
+
+            //Consejo diario, Acciones de mejora.
+            writer.append("Usa decoraciones reutilizables y duraderas.;Planifica el evento con la sostenibilidad en mente, minimizando el uso de recursos y maximizando la eficiencia.\n");
+            writer.append("Elige vajilla y cubiertos compostables.;Implementa una política de cero residuos, incentivando a los participantes a generar la menor cantidad de desechos posible.\n");
+            writer.append("Proporciona contenedores de reciclaje visibles.;Elige un lugar que apoye tus esfuerzos de reciclaje y tenga facilidades para la separación y recolección de residuos.\n");
+            writer.append("Evita los productos de un solo uso.;Contrata proveedores que compartan tus valores de sostenibilidad y puedan proporcionar productos y servicios ecológicos.\n");
+            writer.append("Digitaliza las invitaciones y programas.;Proporciona estaciones de reciclaje claramente marcadas y accesibles, asegurándote de que los asistentes sepan dónde depositar sus residuos.\n");
+            writer.append("Donar sobras de comida a bancos de alimentos.;Incentiva a los asistentes a traer sus propios utensilios reutilizables, como botellas de agua y bolsas de tela.\n");
+            writer.append("Recicla las tarjetas de identificación.;Utiliza tecnología para reducir la necesidad de materiales físicos, como aplicaciones para compartir información del evento.\n");
+            writer.append("Promueve el transporte compartido o público.;Realiza un seguimiento de los residuos generados durante el evento para identificar oportunidades de mejora en futuros eventos.\n");
+            writer.append("Contrata servicios de catering sostenibles.;Comunica tus esfuerzos de sostenibilidad a los asistentes, creando conciencia y fomentando su participación activa.\n");
+            writer.append("Educa a los asistentes sobre reciclaje.;Considera la contratación de un profesional en gestión de residuos para asegurar el manejo adecuado de los desechos generados.\n");
             writer.flush();
             writer.close();
         } catch (IOException e) {
--- app/src/main/java/com/atenea/ecoevent/TipsActivity.java
+++ app/src/main/java/com/atenea/ecoevent/TipsActivity.java
@@ -6,10 +6,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class TipsActivity extends AppCompatActivity {
 
@@ -17,6 +25,38 @@ public class TipsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tips);
+
+        //Referencias a elementos de la vista.
+        TextView dailyTV = findViewById(R.id.tv_daily);
+        TextView improveTV = findViewById(R.id.tv_improve);
+
+        //Leer el archivo que contiene los consejos a mostrar.
+        File dataTips = new File(getFilesDir(), "tips.txt");
+        ArrayList<String> tips = new ArrayList<>();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(dataTips));
+            String lineContent;
+
+            while ((lineContent = reader.readLine()) != null) {
+                //Agregar a la lista el contenido del archivo.
+                tips.add(lineContent);
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Mostrar los consejos en la vista de forma aleatoria.
+        Random random = new Random();
+        Integer randomIndex = random.nextInt(tips.size());
+
+        String dailyTip = tips.get(randomIndex).split(";")[0];
+        String improveTip = tips.get(randomIndex).split(";")[1];
+
+        dailyTV.setText(dailyTip);
+        improveTV.setText(improveTip);
 
         //Configuración de la barra inferior.
         BottomAppBar bottomAppBar = findViewById(R.id.bottom_app_bar);
